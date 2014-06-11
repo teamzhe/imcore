2c1563e41811865e86c8b150a47c2597156268b9
teamzhe <imcore5@zheng-zhen-long.local>
2014-06-11 15:38:33 +0800

    add

---
+++ objectone/objectone/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objectone
+//
+//  Created by zheng zhenlong on 14-6-11.
+//  Copyright (c) 2014年 zheng zhenlong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
